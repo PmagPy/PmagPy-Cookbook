50123b794e4b7d8b203fa8b5a740fcafdd33603c
Lori J <mintblue87@gmail.com>
2018-09-27 10:55:35 -0700

    add “install cartopy” to all install sections

---
+++ typset_install_html.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+htlatex osx_pip "html,1,sections+"
+htlatex windows_pip "html,1,sections+"
+htlatex linux_pip "html,1,sections+"
+htlatex osx_developer "html,1,sections+"
+htlatex windows_developer "html,1,sections+"
+htlatex linux_developer "html,1,sections+"
